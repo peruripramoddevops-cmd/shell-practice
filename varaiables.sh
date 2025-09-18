110fe8dd01e6187cea77ccf73379c392598c3f57
pramod <peruripramoddevops@gmail.com>
2025-09-19 04:53:25 +0530

    some message

--- varaiables.sh
+++ varaiables.sh
@@ -4,4 +4,4 @@ PERSON1=john
 PERSON=Kiran
 
 echo "$PERSON1:: Hi $PERSON2, How are you?"
-echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
