e5e790e133f5a6393cd5dc954a6e086643d1ff24
pramod <peruripramoddevops@gmail.com>
2025-09-21 00:18:42 +0530

    Var

--- special.sh
+++ special.sh
@@ -1,4 +1,11 @@
 #!bin/bash
 
-echo "All Variables passed to the cript: $@"
-echo "All Variables passed to the script:$*"
+echo "Script name $0"
+echo "Current Directory $PWD"
+echo "who is running this $USER"
+echo "Home Directory $HOME"
+echo "PID of this script $$"
+
+sleep 50 &
+
+echo "PID of last command is background is :$!"
