7f003f0cbf34c87167b4e4e0c468c3c3c6fa5d07
pramod <peruripramoddevops@gmail.com>
2025-09-21 00:28:05 +0530

    Var

---
+++ Datatypes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NUMBER1=100
+NUMBER2=200
+
+SUM=(($NUMBER1+$NUMBER2))
+
+echo "sum is $SUM"
+
+Echo "Below are array ones"
+
+LIST=("modi""trump""CBN")
+
+echo "ALL LEADERS are ${LIST[@]}"
+echo "Telugu leader is ${LIST[2]}"
