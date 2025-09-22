22e1c92d8c46439603e9a37a23a592075b0312a9
pramod <peruripramoddevops@gmail.com>
2025-09-22 22:33:00 +0530

    22-09-2025

--- prime.sh
+++ prime.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-
-echo "enter a number"
+echo "Enter a number:"
 read num
 
-if [ $num -lt 2 ]; then
-   echo "$num is not a prime number"
-   exit 0
+# Numbers less than 2 are not prime
+if [ "$num" -lt 2 ]; then
+  echo "$num is not a prime number."
+  exit 0
 fi
 
 is_prime=true
 
-for ((i=2; i*i < $num; i++)); do
-   if [ $num % i -eq 0 ]; then
+# Check for divisibility from 2 up to the square root of the number
+# For simplicity, this script checks up to num/2
+for (( i=2; i*i <= num; i++ )); do
+  if (( num % i == 0 )); then
     is_prime=false
     break
-   fi
+  fi
 done
 
 if "$is_prime"; then
