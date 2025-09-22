a8880153a717a277debd45110cad27de60adb863
pramod <peruripramoddevops@gmail.com>
2025-09-22 22:27:16 +0530

    22-09-2025

---
+++ prime.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+echo "enter a number"
+read num
+
+if [ num -lt 2]; then
+   echo "$num is not a prime number"
+   exit 0
+fi
+
+is_prime=true
+
+for ((i=2; i*i < num; i++)); do
+   if (num % i == 0); then
+    is_prime=false
+    break
+   fi
+done
+
+if "$is_prime"; then
+  echo "$num is a prime number."
+else
+  echo "$num is not a prime number."
+fi
